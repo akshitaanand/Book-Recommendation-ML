515275417350ff1adafbf4c8f5ed4e688eff58a5
akshitaanand <63819293+akshitaanand@users.noreply.github.com>
2022-11-21 00:51:21 -0600

    ML training & UI

---
+++ main.py
@@ -0,0 +1,77 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+books_dict = pickle.load(open('bookspkl.pkl','rb'))
+books = pd.DataFrame(books_dict)
+
+st.title('Books Recommender System')
+
+
+similarity = pickle.load(open('similaritypkl.pkl','rb'))
+def recommend(book):
+    book_index = books[books['title'] == book].index[0]
+    distances = similarity[book_index]
+    books_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:10]
+
+    recommended_books = []
+    recommended_book_posters = []
+    recommended_book_author = []
+    recommended_book_rating = []
+
+    for i in books_list:
+             if(type(books.iloc[i[0]].thumbnail) != str):
+                continue
+             recommended_books.append(books.iloc[i[0]].title)
+             recommended_book_posters.append(books.iloc[i[0]].thumbnail)
+             recommended_book_author.append(books.iloc[i[0]].authors)
+             recommended_book_rating.append(books.iloc[i[0]].average_rating)
+    return recommended_books, recommended_book_posters, recommended_book_author, recommended_book_rating
+
+
+book_dict = pickle.load(open('books.pkl','rb'))
+books = pd.DataFrame(book_dict)
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+
+selected_book_name = st.selectbox(
+    "Type or select a book from the dropdown",
+    books['title'].values
+)
+
+if st.button('Show Recommendation'):
+    names,posters, authors, ratings = recommend(selected_book_name)
+
+    #display with the columns
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.subheader(names[0])
+        st.text("Author: " + str(authors[0]))
+        st.text("Rating: " + str(ratings[0]))
+        st.image(posters[0])
+    with col2:
+        st.subheader(names[1])
+        st.text("Author: " + str(authors[1]))
+        st.text("Rating: " + str(ratings[1]))
+        st.image(posters[1])
+    with col3:
+        st.subheader(names[2])
+        st.text("Author: " + str(authors[2]))
+        st.text("Rating: " + str(ratings[2]))
+        st.image(posters[2])
+
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader(names[3])
+        st.text("Author: " + str(authors[3]))
+        st.text("Rating: " + str(ratings[3]))
+        st.image(posters[3])
+    with col2:
+        st.subheader(names[4])
+        st.text("Author: " + str(authors[4]))
+        st.text("Rating: " + str(ratings[4]))
+        st.image(posters[4])
+
+
+
---
+++ training.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pandas as pd
+import pickle
+
+#read csv file
+books = pd.read_csv("./datasets/books.csv")
+# print(books.head(10))
+
+# print(books.shape)
+
+#cleaning data
+books.drop_duplicates()
+books.dropna(axis='columns')
+
+books.dropna()
+
+books = books[[
+  'title', 'authors', 'categories', 'thumbnail', 'description',
+  'average_rating'
+]]
+# print(books.head())
+
+#creating book tags
+books['identify'] = books['categories'] + " " + books['description']
+
+new_books = books[[
+  'title', 'authors', 'average_rating', 'thumbnail', 'identify'
+]]
+print(new_books.head())
+# print(new_books['identify'][0])
+
+new_books['identify'] = new_books['identify'].apply(lambda x: str(x))
+# print(new_books['identify'][0])
+
+new_books['identify'] = new_books['identify'].apply(lambda x: x.lower())
+# print(new_books['identify'][0])
+
+# building a model
+from sklearn.feature_extraction.text import CountVectorizer
+
+cv = CountVectorizer(max_features=5000, stop_words='english')
+
+vectors = cv.fit_transform(new_books['identify']).toarray()
+# print(vectors)
+
+# print(cv.get_feature_names_out())
+
+#find similar words to recommend to users
+
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+
+#remmove unnecesary endings from words
+def rem_comm(message):
+  phrase = []
+  for item in message.split():
+    phrase.append(ps.stem(item))
+  return " ".join(phrase)
+
+
+new_books['identify'] = new_books['identify'].apply(rem_comm)
+# print(new_books['identify'])
+
+
+# pickle.dump(new_books.to_dict(),open('bookspkl.pkl','wb'))
+
+#calculate distance between each movie to identify the level of similarity
+from sklearn.metrics.pairwise import cosine_similarity
+
+cosine_similarity(vectors)
+
+similarity = cosine_similarity(vectors)
+
+# pickle.dump(similarity,open('similaritypkl.pkl','wb'))
+
+# print(len(similarity))
+
+# print(similarity[0])
+
+
+#sorting according to similarity scores and recommending books
+def recommend(book):
+
+  #find the index of the books
+  book_index = new_books[new_books['title'] == book].index[0]
+  distances = similarity[book_index]
+  books_list = sorted(list(enumerate(distances)),
+                      reverse=True,
+                      key=lambda x: x[1])[1:7]
+
+  #to fetch books from indeces
+  for i in books_list:
+    print(new_books.iloc[i[0]].title)
+
+
+print(recommend('Gilead'))
